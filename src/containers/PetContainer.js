e30ce8a6e1c3b06afc42e2d9f8b71817fad53234
kevinneyer <kevinneyer@gmail.com>
2020-07-23 16:17:27 -0400

    initial set up

---
+++ src/containers/PetContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PetCard from '../components/PetCard'
+
+const PetContainer = () => {
+    return (
+        <div>
+          <PetCard />
+        </div>
+    )
+}
+
+export default PetContainer
