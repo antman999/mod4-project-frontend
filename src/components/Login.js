5a070ab0f7ea1ae53bd5c327c5b1db6fdd22a781
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-07-28 16:56:13 -0400

    login started

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,48 +5,135 @@ class Login extends React.Component {
 		newUser: false,
 		email: '',
 		password: '',
-		confirmAcc: '',
+		passwordC: '',
 		firstName: '',
 		lastName: '',
 		petPreference: '',
 		country: '',
 		city: '',
 		zip: null,
-		bio: '',
-	};
-
-	handleNewUser = () => {
-		this.setState(prevState => ({
-			newUser: !prevState.newUser,
-			email: '',
-			password: '',
-			confirmAcc: '',
-			firstName: '',
-			lastName: '',
-			petPreference: '',
-			country: '',
-			city: '',
-			zip: null,
-			bio: '',
-		}));
+        bio: ''
     };
     
-    handleChange = e => {
-        this.setState({[e.target.name]:e.target.value})
-    }
+   
 
-    
+	// handleNewUser = () => {
+	// 	this.setState(prevState => ({
+	// 		newUser: !prevState.newUser,
+	// 		email: '',
+	// 		password: '',
+	// 		passwordC: '',
+	// 		firstName: '',
+	// 		lastName: '',
+	// 		petPreference: '',
+	// 		country: '',
+	// 		city: '',
+	// 		zip: null,
+	// 		bio: '',
+	// 	}));
+	// };
+
+    handleChange = e => {
+        console.log(e.target.value)
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		if (this.state.password === this.state.passwordC) {
+			fetch('http://localhost:3001/api/v1/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify({
+					email: this.state.email,
+					password_digest: this.state.password,
+					first_name: this.state.firstName,
+					last_name: this.state.last_name,
+					pet_preference: this.state.petPreference,
+					country: this.state.country,
+					city: this.state.city,
+					zip: this.state.zip,
+				}),
+			})
+				.then(res => res.json())
+				.then(response => {
+					if (response.errors) {
+						alert(response.errors);
+                    } else {
+                     
+							// // send them somewhere
+							// // storing the user object SOMEWHERE
+							this.props.setUser(response);
+					}
+				});
+		} else {
+			alert("Passwords don't match!");
+		}
+	};
 
 	render() {
 		return (
 			<div>
 				<h3 className='login-header'>Login!</h3>
-				<form className='login-form'>
-					<input type='text' placeholder='Username' />
-					<br></br>
-					<input type='password' placeholder='Password' />
-					<br></br>
-					<button type='button' class='btn btn-primary'>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						name='email'
+						value={this.state.email}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='Email'
+					/>
+				
+					<input
+						name='firstName'
+						value={this.state.firstName}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='first name...'
+					/>
+					<input
+						name='lastName'
+						value={this.state.lastName}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='last name...'
+					/>
+
+					<input
+						name='country'
+						value={this.state.country}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='Country'
+					/>
+					<input
+						name='city'
+						value={this.state.city}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='city'
+					/>
+					<input
+						name='password'
+						value={this.state.password}
+						onChange={this.handleChange}
+						type='text'
+						placeholder='password'
+					/>
+
+					<input
+						name='passwordC'
+						value={this.state.passwordC}
+						onChange={this.handleChange}
+						type='text'
+						placeholder=' confirm password'
+					/>
+				
+					<button type='submit' class='btn btn-primary'>
 						Login
 					</button>
 				</form>
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -14,34 +14,34 @@ import CreatePet from '../components/CreatePet'
 
 
 class HomeContainer extends Component {
+	state = {
+		pets: [],
+		token: '',
+    filterBy: '',
+    currentUser:null
+	};
 
-    state = {
-      pets: [],
-	  token:'',
-	  filterBy: ''
-	  }
-	
-	  componentDidMount(){
-      fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          "grant_type": "client_credentials", "client_id": '3ZzjR0Nnn3z0kJL2kUa9nIZFOkf5qmGUDsEAWfcEdhQvNWfjJN', "client_secret": '0B9CuxL5JNVl9DcTIOmCzvDRsK6mpzSQijJReNCp'
-        }),
-      })
-				.then(res => res.json())
-        .then(token => {
-          this.setState({ token: token }, () => this.getPets());
-        });
-        
-    }
-  
-  
-  getPets = () => {
-    fetch('https://api.petfinder.com/v2/animals', {
+	componentDidMount() {
+		fetch('https://api.petfinder.com/v2/oauth2/token', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				grant_type: 'client_credentials',
+				client_id: '3ZzjR0Nnn3z0kJL2kUa9nIZFOkf5qmGUDsEAWfcEdhQvNWfjJN',
+				client_secret: '0B9CuxL5JNVl9DcTIOmCzvDRsK6mpzSQijJReNCp',
+			}),
+		})
+			.then(res => res.json())
+			.then(token => {
+				this.setState({ token: token }, () => this.getPets());
+			});
+	}
+
+	getPets = () => {
+		fetch('https://api.petfinder.com/v2/animals', {
 			method: 'GET',
 			headers: {
 				Accept: 'application/json',
@@ -53,53 +53,83 @@ class HomeContainer extends Component {
 			.then(data => {
 				this.setState({ pets: data.animals });
 			});
-  }
-  
+	};
 
-	addPet = (pet) => {
+	addPet = pet => {
 		this.setState({
-			pets: [...this.state.pets, pet ]
-		})
-	}
+			pets: [...this.state.pets, pet],
+		});
+	};
 
-	filterHandler = (e) => {
+	filterHandler = e => {
 		this.setState({
-			filterBy: e.target.name
-		})
-	}
+			filterBy: e.target.name,
+		});
+	};
 
-  render() {
-	  let filteredPets = [...this.state.pets]
-      if(this.state.filterBy === 'dogs'){
-		  filteredPets = filteredPets.filter(pet => pet.type === 'Dog')
-	  }
-	  else if(this.state.filterBy === 'cats'){
-		filteredPets = filteredPets.filter(pet => pet.type === 'Cat')
-	 }
-	 else if(this.state.filterBy === 'birds'){
-		filteredPets = filteredPets.filter(pet => pet.type === 'Bird')
-	 }
-	 else if(this.state.filterBy === 'all'){
-		filteredPets = filteredPets
-	 }
- 	return (
-	  <div>
-		<NavBar 
-		filterBy={this.state.filterBy}
-		filterHandler={this.filterHandler}/>
-		<PetsContainer />
-		<Router>
-			<Route exact path='/pets/:id' render={routerProps => (<PetPage pets={this.state.pets} {...routerProps}/>)} />
-			<Route exact path='/pets' render={routerProps => (<PetCard pets={filteredPets} {...routerProps}/>)} />
-			<Route path='/about' component={About} />
-			<Route path='/contact'render={routerProps => <ContactForm {...routerProps} />} />
-			<Route exact path='/' component={Home} />
-			<Route exact path='/login' component={Login} />
-			<Route exact path='/create' render={routerProps => <CreatePet {...routerProps} addPet={this.addPet} />} /> 
-		</Router>
-	  </div>
-    )
-  }
+  setUser = user => {
+    console.log(this.props)
+    this.setState({ currentUser: user }, () => {
+      this.props.history.push('/pets');
+    }	);
+	};
+// 
+	render() {
+		let filteredPets = [...this.state.pets];
+		if (this.state.filterBy === 'dogs') {
+			filteredPets = filteredPets.filter(pet => pet.type === 'Dog');
+		} else if (this.state.filterBy === 'cats') {
+			filteredPets = filteredPets.filter(pet => pet.type === 'Cat');
+		} else if (this.state.filterBy === 'birds') {
+			filteredPets = filteredPets.filter(pet => pet.type === 'Bird');
+		} else if (this.state.filterBy === 'all') {
+			filteredPets = filteredPets;
+    }
+    console.log(this.state.currentUser)
+		return (
+			<div>
+				<NavBar
+					filterBy={this.state.filterBy}
+					filterHandler={this.filterHandler}
+				/>
+				<PetsContainer />
+				<Router>
+					<Route
+						exact
+						path='/pets/:id'
+						render={routerProps => (
+							<PetPage pets={this.state.pets} {...routerProps} />
+						)}
+					/>
+					<Route
+						exact
+						path='/pets'
+						render={routerProps => (
+							<PetCard pets={filteredPets} {...routerProps} />
+						)}
+					/>
+					<Route path='/about' component={About} />
+					<Route
+						path='/contact'
+						render={routerProps => <ContactForm {...routerProps} />}
+					/>
+					<Route exact path='/' component={Home} />
+					<Route
+						exact
+						path='/login'
+						render={routerProps =>( <Login setUser={this.setUser} />)}
+					/>
+					<Route
+						exact
+						path='/create'
+						render={routerProps => (
+							<CreatePet {...routerProps} addPet={this.addPet} />
+						)}
+					/>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default HomeContainer
