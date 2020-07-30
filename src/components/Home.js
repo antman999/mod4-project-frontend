ee5cef520da7d8e073a8cde7572368a97902f068
kevinneyer <kevinneyer@gmail.com>
2020-07-30 18:40:39 -0400

    kev thursday evening

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return(
         <div>
-          <h1 className='home-text'>Welcome to Our Site!</h1>
+          <h1 className='home-text'>Welcome to Second Chances!</h1>
           <img className='home-image' src="https://atlantahumane.org/wp-content/uploads/2020/06/Adopt-a-Dog-Page-Header-Image.jpg" alt="image" />
         </div>
     )
